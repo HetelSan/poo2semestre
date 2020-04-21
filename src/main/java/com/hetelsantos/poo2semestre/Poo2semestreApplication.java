6ba01c5e31f78287f8a762f378d787986c3b196c
Hetel Santos <hetel.santos@gmail.com>
2020-04-21 11:12:39 -0300

    Projeto criado

---
+++ src/main/java/com/hetelsantos/poo2semestre/Poo2semestreApplication.java
@@ -0,0 +1,13 @@
+package com.hetelsantos.poo2semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poo2semestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poo2semestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hetelsantos/poo2semestre/Poo2semestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hetelsantos.poo2semestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poo2semestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
